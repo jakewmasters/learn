29c0422239ff599f9bbdcf976504c4c949aa8509
jakewmasters <jakewmasters@gmail.com>
2018-05-30 16:59:21 -0500

    Added maths!

--- example.go
+++ example.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"math/cmplx"
 	"time"
 )
@@ -23,7 +25,19 @@ func print_types(){
 	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
 
+func print_math(){
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+	fmt.Println(math.Pi)
+}
+
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	print_time()
 	print_types()
+	print_math()
+	fmt.Println(add(42, 13))
 }
